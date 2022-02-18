bc8da90943605d401e394d88450759bc5100fce5
Shawn Marshall-Spitzbart <smarshall-spitzbart@berkeley.edu>
2022-02-18 00:47:19 -0800

    test stuff

--- test/Arbitrage.js
+++ test/Arbitrage.js
@@ -1,16 +1,13 @@
 const { expect } = require("chai");
 
 describe("Arbitrage contract", function () {
-  it("Need to write some tests", async function () {
+  it("test WETH market id, just a start here.", async function () {
     const [owner] = await ethers.getSigners();
-
-    const contract = await ethers.getContractFactory("DyDxFlashLoan");
-
-    const hardhatToken = await contract.deploy();
-
-    expect(true);
-
-    // const ownerBalance = await hardhatToken.balanceOf(owner.address);
-    // expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+    const flashLoanContract = await ethers.getContractFactory("DyDxFlashLoan");
+    // With hardhat-ethers plugin, contract is deployed to first signer by default.
+    const deployedContract = await flashLoanContract.deploy();
+    const wethAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+    const marketId = await deployedContract.tokenToMarketId(wethAddress);
+    expect(marketId).to.equal(0);
   });
 });
