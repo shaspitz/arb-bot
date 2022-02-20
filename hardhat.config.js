77fa050ce5a5f13d869bd7efb386b2fb7f5c781b
Shawn Marshall-Spitzbart <smarshall-spitzbart@berkeley.edu>
2022-02-19 19:11:14 -0800

    specify block number

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,4 @@
 require("@nomiclabs/hardhat-waffle");
-
 require('dotenv').config();
 
 /**
@@ -11,6 +10,7 @@ module.exports = {
     hardhat: {
       forking: {
         url: "https://eth-mainnet.alchemyapi.io/v2/" + process.env.ALCHEMY_API_KEY,
+        blockNumber: 14240415, // Specify block number for performance, see https://hardhat.org/hardhat-network/guides/mainnet-forking.html.
       }
     }
   }
