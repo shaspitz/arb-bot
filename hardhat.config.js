87b932b2e4e2e9bdb873db36dd05a26ca80aab61
Shawn Marshall-Spitzbart <smarshall-spitzbart@berkeley.edu>
2022-02-19 19:07:30 -0800

    forking setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,17 @@
 require("@nomiclabs/hardhat-waffle");
 
+require('dotenv').config();
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.0", 
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://eth-mainnet.alchemyapi.io/v2/" + process.env.ALCHEMY_API_KEY,
+      }
+    }
+  }
 };
