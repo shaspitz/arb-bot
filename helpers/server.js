4b24324ceb92ce01e106485407c900d8bc9d7efb
Shawn Marshall-Spitzbart <smarshall-spitzbart@berkeley.edu>
2022-03-07 22:17:06 -0800

    Update server.js

--- helpers/server.js
+++ helpers/server.js
@@ -4,8 +4,9 @@ const http = require('http')
 const cors = require('cors')
 
 // SERVER CONFIG
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 const app = express();
-const server = http.createServer(app).listen(PORT, () => console.log(`Listening on ${PORT}\n`))
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(cors({ credentials: true, origin: '*' }))
+const server = http.createServer(app).listen(PORT, () => console.log(`Listening on ${PORT}\n`));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({ credentials: true, origin: '*' }));
+
