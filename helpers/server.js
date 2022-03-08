c182dc7d3d33bd8016ed8f86e6cfb6300bdddb87
Shawn Marshall-Spitzbart <smarshall-spitzbart@berkeley.edu>
2022-03-08 00:10:11 -0800

    Update server.js

--- helpers/server.js
+++ helpers/server.js
@@ -1,12 +1,12 @@
+// Responsible for spinning up a local server for the bot.
 const express = require('express');
 const path = require('path');
 const http = require('http');
 const cors = require('cors');
 
-// SERVER CONFIG
+// Server config.
 const PORT = process.env.PORT || 5000;
 const app = express();
 const server = http.createServer(app).listen(PORT, () => console.log(`Listening on ${PORT}\n`));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({ credentials: true, origin: '*' }));
-
